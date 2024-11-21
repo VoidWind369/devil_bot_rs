8e5c6ab968326114c6296a06b1f5452e1990f231
VoidWind <voidwind@qq.com>
2024-11-21 19:27:58 +0800

    更新多群

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use rmp_serde::Serializer;
+use serde::Serialize;
 use void_log::*;
 
 mod start;
 mod api;
 mod link;
 mod util;
+mod msg_pack;
 
 #[tokio::main]
 async fn main() {
@@ -19,4 +22,14 @@ async fn main() {
     loop {
         link::conn().await;
     }
+}
+
+#[tokio::test]
+async fn test() {
+    let mut buf = Vec::new();
+    let mp = msg_pack::Message::new("虚無风", "测试cececec");
+    mp.serialize(&mut Serializer::new(&mut buf)).unwrap();
+    log_info!("{:?}", buf);
+
+    reqwest::Client::new().post("").body(buf).send().await.unwrap();
 }
