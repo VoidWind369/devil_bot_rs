3aa05e1389c69f857f4fc2cd9c2e979fe39c0e9e
VoidWind <voidwind@qq.com>
2024-11-27 01:30:14 +0800

    更新多群

--- src/controller/image_api.rs
+++ src/controller/image_api.rs
@@ -1,3 +1,4 @@
+use std::io::Cursor;
 use crate::om_api::record::Record;
 use axum::extract::Path;
 use axum::http::header::CONTENT_TYPE;
@@ -5,15 +6,22 @@ use axum::http::{HeaderMap, HeaderValue};
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
+use image::ImageFormat;
 
 async fn get_record(Path(tag): Path<String>) -> impl IntoResponse {
-    let data = Record::new(&tag, 0).await.list_img(50).await.into_bytes();
+    let data = Record::new(&tag, 0).await.list_img(50).await;
+
+    // 创建一个内存中的缓冲区（Cursor<Vec<u8>）
+    let mut buffer = Cursor::new(Vec::new());
+    // 将图像数据写入缓冲区
+    data.write_to(&mut buffer, ImageFormat::Png).unwrap();
+
     let mut headers = HeaderMap::new();
     headers.insert(
         CONTENT_TYPE,
         HeaderValue::from_static("image/png"),
     );
-    (headers, data).into_response()
+    (headers, buffer.into_inner()).into_response()
 }
 
 pub fn router(app: Router) -> Router {
