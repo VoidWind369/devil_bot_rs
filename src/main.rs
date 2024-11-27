7271dbfbe05a05b2ae5d81f918927b036500cbc3
mzx <voidwind@qq.com>
2024-11-27 09:37:47 +0800

    bot-rs

--- src/main.rs
+++ src/main.rs
@@ -38,7 +38,7 @@ async fn main() {
 
 #[tokio::test]
 async fn test() {
-    let a = om_api::record::Record::new("#Y00UQJPJ", "1329997614", 0).await;
+    let a = om_api::record::Record::new("#Y00UQJPJ", 0).await;
     log_info!("{:?}", &a);
     a.list_img(30).await.save("p.png").unwrap();
 }
